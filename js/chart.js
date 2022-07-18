718fecf51c979c609181fc4e9c032135cc39fdf1
DDakbin <108514639+DDakbin@users.noreply.github.com>
2022-07-18 19:50:13 +0900

    Add files via upload

---
+++ js/chart.js
@@ -0,0 +1,65 @@
+
+// let number = document.getElementById("number-ps");
+// let number1 = document.getElementById("number-ai");
+// let number2 = document.getElementById("number-html");
+// let number3 = document.getElementById("number-js");
+// let counter = 0;
+// let html_counter = 0;
+// let js_counter = 0;
+
+
+// anime({
+//     targets:'.st0',
+//     strokeDashoffset:[anime.setDashoffset, 0],
+//     duration:4000,
+//     easing:'easeOutSine'
+// });
+
+// anime({
+//     targets:'.st1',
+//     strokeDashoffset:[anime.setDashoffset, 0],
+//     duration:4000,
+//     easing:'easeOutSine'
+// });
+
+// anime({
+//     targets:'.st2',
+//     strokeDashoffset:[anime.setDashoffset, 0],
+//     duration:4000,
+//     easing:'easeOutSine'
+// });
+
+// anime({
+//     targets:'.st3',
+//     strokeDashoffset:[anime.setDashoffset, 0],
+//     duration:4000,
+//     easing:'easeOutSine'
+// });
+
+// setInterval(function(){
+//     if(counter == 90){
+//         clearInterval();
+//     }else{
+//         counter += 1;
+//         number.innerHTML = '<img src="images/photoshop_logo.jpg">' + "<br>" + counter + "%" ;
+//         number1.innerHTML = '<img src="images/illust_logo.jpg">' + "<br>" + counter + "%" ;
+//     }
+// },40);
+
+// setInterval(function(){
+//     if(html_counter == 70){
+//         clearInterval();
+//     }else{
+//         html_counter += 1;
+//         number2.innerHTML = '<img src="images/html_css_logo.png">' + "<br>" + html_counter + "%" ;
+//     }
+// },47);
+
+// setInterval(function(){
+//     if(js_counter == 60){
+//         clearInterval();
+//     }else{
+//         js_counter += 1;
+//         number3.innerHTML = '<img src="images/js_logo.png">' + "<br>" + js_counter + "%" ;
+//     }
+// },55);
